2020881391cc7c0c157cb9d4c86afc7971fb7580
sridhar <sridharbabuk99@gmail.com>
2018-11-17 11:58:06 +0530

    Third  Chnages

--- bootpract/src/main/java/com/boot/bootpract/BootpractApplication.java
+++ bootpract/src/main/java/com/boot/bootpract/BootpractApplication.java
@@ -2,10 +2,17 @@ package com.boot.bootpract;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class BootpractApplication {
 
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(BootpractApplication.class, args);
 		System.out.println("hello");
